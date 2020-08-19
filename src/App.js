5c72836663484eb270a7d8657dcc9093061944c0
Jon Sanders <jpsanders101@gmail.com>
2020-08-19 09:13:41 +0100

    Add holding message.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1><span role="img" aria-label="Construction sign">🚧 </span>Hi Niamh!<span role="img" aria-label="Construction sign"> 🚧</span><br />Your website is on its way, pal!</h1>
       </header>
     </div>
   );
