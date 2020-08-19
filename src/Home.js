76ca744334835e6f3fbbd38b3e9ea9f372a411cd
Jon Sanders <jpsanders101@gmail.com>
2020-08-19 15:44:24 +0100

    Style homepage.

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Home.css";
 
 export default function Home() {
   return (
     <div>
       <h1>Niamh Gordon</h1>
-      <Link to="/about">About</Link>
+      <ul className="linkList">
+        <li>
+          <Link className="link" to="/about">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/writing">Writing</Link>
+        </li>
+        <li>
+          <Link to="/cv">CV</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
     </div>
   );
 }
