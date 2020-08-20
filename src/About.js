66b363a4abc9001f20944491127772991f68e860
Jon Sanders <jpsanders101@gmail.com>
2020-08-20 21:38:36 +0100

    About page layout.

--- src/About.js
+++ src/About.js
@@ -7,13 +7,14 @@ export default function About() {
     <div className="aboutPage">
       <div className="column">
         <p className="copy">
+          <div className="authorPhoto authorPhotoFloat" />
           Niamh was born in Liverpool and grew up in Manchester. She is a
           writer, and has also been variously a barista, assistant manager of a
           chocolate shop, a tutor, an educational events coordinator, a shipping
           administrator, an editor, a fundraiser, a sales assistant (amongst
           others).
-        </p>
-        <p className="copy">
+          <br />
+          <br />
           In 2018 she completed her MA in Prose Fiction from the University of
           East Anglia, and in October 2020 she will begin an AHRC-funded DFA in
           Creative and Critical Writing at the University of Glasgow. Her
@@ -21,17 +22,18 @@ export default function About() {
           writings of Ali Smith. She also writes about the nature of work,
           trauma’s impact on narrative time, public transport, bereavement by
           suicide, the epistolary internet, and ghosts.
-        </p>
-        <p className="copy">
+          <br />
+          <br />
           She has fiction work forthcoming in Return Trip magazine, and her
           poetry has been longlisted for Issue 14 of Butcher’s Dog magazine. In
           her spare time she likes to run, cook, and work her way through every
           crime drama on Netflix.
         </p>
+
         <HomeLink />
       </div>
       <div className="column">
-        <div className="authorPhoto" />
+        <div className="authorPhoto authorPhotoFlex" />
       </div>
     </div>
   );
