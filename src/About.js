5e05141deed4c2505b091f007d073cce8b3d8e62
Jon Sanders <jonathan.sanders@ourfuturehealth.org.uk>
2024-01-20 20:22:54 +0000

    Add links.

--- src/About.js
+++ src/About.js
@@ -1,30 +1,9 @@
 import React from "react";
-// import HomeLink from "./HomeLink";
 import "./About.css";
 
 export default function About() {
   return (
     <>
-      {/* <div className="aboutPage">
-        <div className="column">
-          <p className="copy">
-            BIO:
-            <br />
-            I am a writer, researcher and editor from Manchester, currently based in Glasgow. I work as a Research Assistant with <a rel="noreferrer" target="_blank" href="https://deathwrites.org.uk/">the DeathWrites Network</a>, and am a volunteer Community Assistant with <a rel="noreferrer" target="_blank" href="https://www.newmumswriting.co.uk/">The New Mother’s Writing Circle</a>. I am also a part-time DFA candidate in Creative Writing at the University of Glasgow, and a parent to a toddler.
-            <br />
-            <br />
-            RESEARCH:
-            <br />
-            My AHRC-funded doctorate investigates the relationships between narrative, time, and trauma, through a practice-based exploration of the impact of bereavement by suicide on narrative time. I also research Ali Smith, and experimental writing. I work across narratology, critical medical humanities, and creative writing. At the University of Glasgow I have taught courses on poetry and poetics, experimental writing, and writing the body. I co-edited the literary magazine <i className="italics">From Glasgow to Saturn</i> from 2020-2021, and currently edit the DeathWrites blog. I am a volunteer editor with the New Mother’s Writing Circle.
-            <br />
-            <br />
-            <div className="authorPhoto authorPhotoFloat" />
-          </p>
-        </div>
-        <div className="column">
-          <div className="authorPhoto authorPhotoFlex" />
-        </div>
-      </div> */}
       <p className="copy">
       BIO
             <br />
@@ -36,26 +15,24 @@ export default function About() {
             RESEARCH
             <br />
             <br />
-            My AHRC-funded doctorate investigates the relationships between narrative, time, and trauma, through a practice-based exploration of the impact of bereavement by suicide on narrative time. I also research Ali Smith, and experimental writing. I work across narratology, critical medical humanities, and creative writing. At the University of Glasgow I have taught courses on poetry and poetics, experimental writing, and writing the body. I co-edited the literary magazine <i className="italics">From Glasgow to Saturn</i> from 2020-2021, and currently edit the DeathWrites blog. I am a volunteer editor with the New Mother’s Writing Circle.
+            My <a href="https://www.gla.ac.uk/pgrs/niamhgordon/" target="_blank"  rel="noreferrer">AHRC-funded doctorate</a> investigates the relationships between narrative, time, and trauma, through a practice-based exploration of the impact of bereavement by suicide on narrative time. I also research Ali Smith, and experimental writing. I work across narratology, critical medical humanities, and creative writing. At the University of Glasgow I have taught courses on poetry and poetics, experimental writing, and writing the body. I co-edited the literary magazine <a rel="noreferrer" href="https://glasgowtosaturn.com/" target="_blank" className="italics">From Glasgow to Saturn</a> from 2020-2021, and currently edit the <a href="https://deathwrites.org.uk/blog" target="_blank" rel="noreferrer">DeathWrites blog</a>.
             <br />
             <br />
             <br />
         WRITING
         <br />
         <br />
-        I have published short fiction, poetry, and essays – linked below. My novel-in-progress was shortlisted for the 2021 North Lit Agency prize.
+        I have published short fiction, poetry, and essays – linked below. My novel-in-progress was shortlisted for the <a target="_blank" rel="noreferrer" href="https://www.thebookseller.com/news/clare-gough-wins-north-literary-agency-prize-1262107">2021 North Lit Agency prize</a>.
         <br />
         <ul className="writing-list">
           <li><a rel="noreferrer" target="_blank" href="https://www.strixleeds.com/shop/strix-10">‘Birth lament’ and ‘Dispatch from now’</a> - two poems in Strix #10</li>
           <li><a rel="noreferrer" target="_blank" href="https://english.exeter.ac.uk/media/universityofexeter/collegeofhumanities/english/research/exclamationjournal/Exclamation_Volume_6.pdf">‘Allopoeitics as criticism’</a> – peer-reviewed experimental essay in Exclamat!on, Issue 6</li>
           <li><a rel="noreferrer" target="_blank" href="https://thepolyphony.org/2021/11/11/grief-reading-and-narrative-time/">‘Grief, reading, and narrative time’</a> - essay on The Polyphony</li>
-          <li>‘Empty Space’ – short fiction in Still Point Journal, Issue 5: Presence</li>
-          <li>‘Covering Ground in Berlin’ – short fiction in Return Trip, Issue 02</li>
-          <li>‘A Rich Man’s Game’ – flash fiction in Flash Fiction Magazine</li>
+          <li><a href="https://www.stillpointldn.com/articles/niamh-gordon-empty-space/" target="_blank" rel="noreferrer">‘Empty Space’</a> – short fiction in Still Point Journal, Issue 5: Presence</li>
+          <li><a href="https://returntrip.bigcartel.com/product/return-trip-spring-summer-2021" target="_blank" rel="noreferrer">‘Covering Ground in Berlin’</a> – short fiction in Return Trip, Issue 02</li>
+          <li><a target="_blank" href="https://flashfictionmagazine.com/blog/2020/10/01/a-rich-mans-game/" rel="noreferrer">‘A Rich Man’s Game’</a> – flash fiction in Flash Fiction Magazine</li>
         </ul>
       </p>
-      {/* <HomeLink /> */}
-
     </>
   );
 }
